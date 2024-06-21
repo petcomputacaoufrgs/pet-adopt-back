f58b3046cbeecdfa06e1fac7512f13001dc7f80c
Maximus Rosa <maxborgesrosa@gmail.com>
2024-06-21 12:43:17 -0300

    entidade membro-ong criada

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PetModule } from './domain/pet/pet.module';
+import { MembroOngModule } from './domain/membro-ong/membro-ong.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PetModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PetModule, MembroOngModule],
   controllers: [],
   providers: [],
 })
