74f17dc42891b9dbc793b795fd640e71439e6a2b
Maximus Rosa <maxborgesrosa@gmail.com>
2024-06-21 12:45:45 -0300

    Revert "entidade membro-ong criada"
    This reverts commit f58b3046cbeecdfa06e1fac7512f13001dc7f80c.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PetModule } from './domain/pet/pet.module';
-import { MembroOngModule } from './domain/membro-ong/membro-ong.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PetModule, MembroOngModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PetModule],
   controllers: [],
   providers: [],
 })
--- src/domain/membro-ong/membro-ong.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('membro-ong')
-export class MembroOngController {}
